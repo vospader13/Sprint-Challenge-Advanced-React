74e6b2bd2f36724f07362d33c450a1816abb7e75
vospader13 <justenmcdonald@gmail.com>
2020-04-17 11:00:42 -0600

    Completed project with a few tests.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,30 +1,52 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import ReactDOM from 'react-dom';
 import App from './App';
-// 
-it('renders without crashing', () => {
+import { Navbar } from './components/Navbar';
+import { PlayerList } from './components/PlayerList';
+import { PlayerCard } from './components/PlayerCard';
+import UseDarkMode, { darkMode, setDarkMode} from './Hooks/UseDarkMode';
+
+
+test('renders App without crashing', () => {
   
   render(<App />);
   
 });
 
+test('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
 
+it("making sure the darkMode state is working", () => {
+  expect(darkMode).toBeInTheDocument;
+})
 
-describe('player list component', () => {
-  it('renders without crashing', () => {
-   const sum = (a,b) => {
-      return a+b;
-  }
-  expect(sum(2,2)).toBe(4)
+describe("the setDarkMode state is working well", ( ) => {
+  it("making sure the setDarkMode state is working", () => {
+    expect(setDarkMode).toBeInTheDocument;
   })
-})
+});
 
 
-test('is dark mode working', () => {
-  const darkmodeBtn = getByText('Dark Mode');
-  const darkModeOn = getByText('Dark mode on!')
-
-  fireEvent.cleck(darkmodeBtn);
-
-  expect(darkModeOn).hasTextContent();
-})
+it('renders "name" text', () => {
+  const { queryByText } = render(
+    <App />
+    );
+    const name = queryByText(/Name/i);
+  });
+it('renders "country" text', () => {
+  const { queryByText } = render(
+    <App />
+    );
+    const country = queryByText(/Country/i);
+  });
+it('renders "searches" text', () => {
+  const { queryByText } = render(
+    <App />
+    );
+    const searches = queryByText(/Searches/i);
+  });
